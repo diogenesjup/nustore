ad4c24ce361b3c08bdd641342e3f456fd56b0b3e
Diogenes Oliveira Junior <contato@diogenesjunior.com.br>
2016-06-11 21:56:49 -0300

    Add files via upload

---
+++ www/server_processing.php
@@ -0,0 +1,18 @@
+{
+    "draw": 1,
+    "recordsTotal": 2,
+    "recordsFiltered": 2,
+    "data": [
+        [
+            "Batata Frita<br><i>Fabricado por Extra</i>",
+            "R$56,00",
+            " "
+        ],
+        [
+            "Macarrão<br><i>Fabricado por Dois de Corda</i>",
+            "R$12,00",
+            " "
+        ]
+        
+    ]
+}
