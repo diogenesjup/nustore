2675bd176097a93a62490ce150c9b6c4e34e1fc3
Diogenes Oliveira Junior <contato@diogenesjunior.com.br>
2016-06-11 22:11:12 -0300

    Add files via upload

--- www/server_processing.php
+++ www/server_processing.php
@@ -1,17 +1,47 @@
 {
     "draw": 1,
-    "recordsTotal": 2,
-    "recordsFiltered": 2,
+    "recordsTotal": 6,
+    "recordsFiltered": 6,
     "data": [
         [
             "Batata Frita<br><i>Fabricado por Extra</i>",
             "R$56,00",
-            " "
+            "<button class='btn btn-primary btn-xs' onclick=''>add carrinho</button>"
         ],
         [
             "Macarrão<br><i>Fabricado por Dois de Corda</i>",
             "R$12,00",
-            " "
+            "<button class='btn btn-primary btn-xs' onclick=''>add carrinho</button>"
+        ],
+        [
+            "Coca Cola<br><i>Fabricado por Coke of America</i>",
+            "R$5,00",
+            "<button class='btn btn-primary btn-xs' onclick=''>add carrinho</button>"
+        ],
+        [
+            "Pepsi<br><i>Fabricado por Pepsico</i>",
+            "R$4,50",
+            "<button class='btn btn-primary btn-xs' onclick=''>add carrinho</button>"
+        ],
+        [
+            "Feijão<br><i>Fabricado por Dois de Corda</i>",
+            "R$12,00",
+            "<button class='btn btn-primary btn-xs' onclick=''>add carrinho</button>"
+        ],
+        [
+            "Sonho de Valsa<br><i>Fabricado por Nestlê</i>",
+            "R$12,00",
+            "<button class='btn btn-primary btn-xs' onclick=''>add carrinho</button>"
+        ],
+        [
+            "Chocolate<br><i>Fabricado por Nestlê</i>",
+            "R$12,00",
+            "<button class='btn btn-primary btn-xs' onclick=''>add carrinho</button>"
+        ],
+        [
+            "Maça<br><i>Fabricado por Turma da Mõnico</i>",
+            "R$12,00",
+            "<button class='btn btn-primary btn-xs' onclick=''>add carrinho</button>"
         ]
         
     ]
