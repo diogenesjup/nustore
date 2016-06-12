32da2c2c8c3b0979d69ad2ed933df10004416b24
Diogenes Oliveira Junior <contato@diogenesjunior.com.br>
2016-06-11 22:46:32 -0300

    Add files via upload

--- www/server_processing.php
+++ www/server_processing.php
@@ -6,42 +6,57 @@
         [
             "Batata Frita<br><i>Fabricado por Extra</i>",
             "R$56,00",
-            "<button class='btn btn-primary btn-xs' onclick=''>add carrinho</button>"
+            "<button class='btn btn-primary btn-xs' onclick='msgAddCarrinho()'>add carrinho</button>"
         ],
         [
             "Macarrão<br><i>Fabricado por Dois de Corda</i>",
             "R$12,00",
-            "<button class='btn btn-primary btn-xs' onclick=''>add carrinho</button>"
+            "<button class='btn btn-primary btn-xs' onclick='msgAddCarrinho()'>add carrinho</button>"
         ],
         [
             "Coca Cola<br><i>Fabricado por Coke of America</i>",
             "R$5,00",
-            "<button class='btn btn-primary btn-xs' onclick=''>add carrinho</button>"
+            "<button class='btn btn-primary btn-xs' onclick='msgAddCarrinho()'>add carrinho</button>"
         ],
         [
             "Pepsi<br><i>Fabricado por Pepsico</i>",
             "R$4,50",
-            "<button class='btn btn-primary btn-xs' onclick=''>add carrinho</button>"
+            "<button class='btn btn-primary btn-xs' onclick='msgAddCarrinho()'>add carrinho</button>"
         ],
         [
             "Feijão<br><i>Fabricado por Dois de Corda</i>",
             "R$12,00",
-            "<button class='btn btn-primary btn-xs' onclick=''>add carrinho</button>"
+            "<button class='btn btn-primary btn-xs' onclick='msgAddCarrinho()'>add carrinho</button>"
         ],
         [
             "Sonho de Valsa<br><i>Fabricado por Nestlê</i>",
             "R$12,00",
-            "<button class='btn btn-primary btn-xs' onclick=''>add carrinho</button>"
+            "<button class='btn btn-primary btn-xs' onclick='msgAddCarrinho()'>add carrinho</button>"
         ],
         [
             "Chocolate<br><i>Fabricado por Nestlê</i>",
             "R$12,00",
-            "<button class='btn btn-primary btn-xs' onclick=''>add carrinho</button>"
+            "<button class='btn btn-primary btn-xs' onclick='msgAddCarrinho()'>add carrinho</button>"
         ],
         [
             "Maça<br><i>Fabricado por Turma da Mõnico</i>",
             "R$12,00",
-            "<button class='btn btn-primary btn-xs' onclick=''>add carrinho</button>"
+            "<button class='btn btn-primary btn-xs' onclick='msgAddCarrinho()'>add carrinho</button>"
+        ],
+        [
+            "Dorflex<br><i>Fabricado por Novalgina</i>",
+            "R$5,00",
+            "<button class='btn btn-primary btn-xs' onclick='msgAddCarrinho()'>add carrinho</button>"
+        ],
+        [
+            "Nescal<br><i>Fabricado por Nestlê</i>",
+            "R$12,00",
+            "<button class='btn btn-primary btn-xs' onclick='msgAddCarrinho()'>add carrinho</button>"
+        ],
+        [
+            "Sucrilhos Kelloks<br><i>Fabricado por Kelloks</i>",
+            "R$16,50",
+            "<button class='btn btn-primary btn-xs' onclick='msgAddCarrinho()'>add carrinho</button>"
         ]
         
     ]
